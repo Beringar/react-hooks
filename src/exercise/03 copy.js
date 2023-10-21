a342799aee9b88b104247574e226b3dca50a9944
Berenguer Pou <berenguer.pou@gmail.com>
2023-10-21 20:33:25 +0200

    exercise 3 extra 1 state colocation

--- src/exercise/03 copy.js
+++ src/exercise/03 copy.js
@@ -3,17 +3,11 @@
 
 import * as React from 'react'
 
-function Name({placeholder = 'write your name'}) {
-  const [name, setName] = React.useState('')
+function Name({name, onNameChange}) {
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input
-        id="name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder={placeholder}
-      />
+      <input id="name" value={name} onChange={onNameChange} />
     </div>
   )
 }
@@ -27,20 +21,21 @@ function FavoriteAnimal({animal, onAnimalChange}) {
   )
 }
 
-function Display({animal}) {
-  return <div>{`Your favorite animal is: ${animal}!`}</div>
+function Display({name, animal}) {
+  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   const [animal, setAnimal] = React.useState('')
+  const [name, setName] = React.useState('')
   return (
     <form>
-      <Name placeholder={'put here your name'} />
+      <Name name={name} onNameChange={event => setName(event.target.value)} />
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={e => setAnimal(e.target.value)}
       />
-      <Display animal={animal} />
+      <Display name={name} animal={animal} />
     </form>
   )
 }
